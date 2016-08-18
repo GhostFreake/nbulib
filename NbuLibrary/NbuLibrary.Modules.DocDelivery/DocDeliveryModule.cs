b561c8d728bad35c9aa19db89759f32dd8159f79
DZoin <dimitar.zoin@gmail.com>
2016-08-18 21:17:37 +0300

    DocDelivery - All included

---
+++ NbuLibrary/NbuLibrary.Modules.DocDelivery/DocDeliveryModule.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NbuLibrary.Core.ModuleEngine;
+using NbuLibrary.Core.Services;
+using NbuLibrary.Core.Services.tmp;
+using Ninject.Modules;
+using NbuLibrary.Core.Domain;
+using System.Security.Cryptography;
+using NbuLibrary.Core.Service.tmp;
+using NbuLibrary.Core.DataModel;
+
+namespace NbuLibrary.Modules.DocDelivery
+{
+    public class Permissions
+    {
+        public const string Use = "Use";
+    }
+
+    public class DocDeliveryNinjectModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IModule>().To<DocDeliveryModule>();
+            Bind<IEntityOperationInspector>().To<EntityOperationInspector>();
+            Bind<IEntityQueryInspector>().To<EntityQueryInspector>();
+            Bind<IEntityOperationLogic>().To<AskTheLibOperationLogic>();
+        }
+    }
+    public class DocDeliveryModule : IModule
+    {
+    }
+
+    public class DocDeliveryUIProvider : IUIProvider
+    {
+
+    }
+
+    public class EntityOperationInspector : IEntityOperationInspector
+    {
+
+    }
+
+    public class AskTheLibOperationLogic : IEntityOperationLogic
+    {
+    }
+}
---
+++ NbuLibrary/NbuLibrary.Modules.DocDelivery/EntityQueryInspector.cs
@@ -0,0 +1,23 @@
+﻿using NbuLibrary.Core.Domain;
+using NbuLibrary.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NbuLibrary.Modules.DocDelivery
+{
+    public class EntityQueryInspector : IEntityQueryInspector
+    {
+        private ISecurityService _securityService;
+
+        public EntityQueryInspector(ISecurityService securityService)
+        {
+            _securityService = securityService;
+
+        }
+
+       
+    }
+}
